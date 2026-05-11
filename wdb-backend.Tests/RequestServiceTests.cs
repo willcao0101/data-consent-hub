995f2b81c4d81d8394b55e52884db9f089bfa3b1
Iris <zhangsi1@myvuw.ac.nz>
2026-05-11 14:34:59 +1200

    test:add unit tests for GetAllByEmployerIdAsync in RequestServiceImpl

--- wdb-backend.Tests/RequestServiceTests.cs
+++ wdb-backend.Tests/RequestServiceTests.cs
@@ -37,4 +37,45 @@ public class RequestServiceTests
         // Act & Assert 
         await Assert.ThrowsAsync<KeyNotFoundException>(() => service.CreateAsync(Guid.NewGuid(), Guid.NewGuid(), reason));
     }
+
+    [Fact]
+    public async Task GetAllByEmployerIdAsync_validRequests_ReturnsAllMatchingRequests()
+    {
+        // Arrange
+        var mockRepo = new Mock<IRequestRepository>();
+        var service = new RequestServiceImpl(mockRepo.Object);
+        var requests = new List<Request>();
+        var employerId = Guid.NewGuid();
+        var workerId1 = Guid.NewGuid();
+        var workerId2 = Guid.NewGuid();
+        var reason1 = "checkWorker1";
+        var reason2 = "checkWorker2";
+        var fakeRequest1 = new Request { EmployerId = employerId, WorkerId = workerId1, Reason = reason1 };
+        var fakeRequest2 = new Request { EmployerId = employerId, WorkerId = workerId2, Reason = reason2 };
+        requests.Add(fakeRequest1);
+        requests.Add(fakeRequest2);
+        mockRepo.Setup(r=>r.GetAllByEmployerIdAsync(employerId,default)).ReturnsAsync(requests);
+        
+        // Act
+        var result = await service.GetAllByEmployerIdAsync(employerId, default);
+
+        // Assert
+        Assert.Equivalent(requests,result);
+    }
+
+    [Fact]
+    public async Task  GetAllByEmployerIdAsync_NoRequests_ReturnsEmptyList()
+    {
+        // Arrange
+        var mockRepo = new Mock<IRequestRepository>();
+        var service = new RequestServiceImpl(mockRepo.Object);
+        var employerId = Guid.NewGuid();
+        mockRepo.Setup(r=>r.GetAllByEmployerIdAsync(employerId,default)).ReturnsAsync((List<Request>?)null);
+        
+        // Act
+        var result = await service.GetAllByEmployerIdAsync(employerId, default);
+
+        // Assert
+        Assert.Empty(result);     
+    }
 }
